cf1b7794883fc503a80e8c038a16c968e2c6e3e5
Vitoria Fontoura <vitoriabfontoura@gmail.com>
2021-05-31 23:34:04 -0300

    update

--- Projeto/IfaceApp.java
+++ Projeto/IfaceApp.java
@@ -24,15 +24,29 @@ class IfaceFrame extends JFrame {
     private String getChecked = "null";
 
     private int j = 0;
+<<<<<<< HEAD
+    private int x1, y1;
+    private Figure focus = null;
+    private Button focus_but = null;
+    private Figure last_focus = null;
+    private Figure buttonMouse = new Ellipse (28,275,10,10,255, 0, 0,0, 0, 0);
+
+=======
 
     private Figure focus = null;
     private Button focus_but = null;
     private Figure last_focus = null;
 
+>>>>>>> d43031c0a3f3361bc4082ad56690f4a9351c60ea
     private ImageIcon imagem = new ImageIcon(getClass().getResource("img/garbage.png"));
     private Image img = imagem.getImage();
     
     private Point prevPt;
+<<<<<<< HEAD
+
+    private boolean positionMouse = false;
+=======
+>>>>>>> d43031c0a3f3361bc4082ad56690f4a9351c60ea
 
     IfaceFrame () {
         try {
@@ -111,12 +125,14 @@ class IfaceFrame extends JFrame {
         			for (Figure fig: figs){
         				if (fig.clicked(evt.getX(), evt.getY())){
                             focus = fig;
+                            if (focus == last_focus){
+                                break;
+                            }
         				}
                     }
 
                     for (Button but: buts){
                         if (but.clicked(evt.getX(), evt.getY())){
-                            //focus = null;
                             focus_but = but;
 
                             if (focus_but.idx == 5 && last_focus != null){
@@ -130,11 +146,12 @@ class IfaceFrame extends JFrame {
                                     list.set(2, (list.get(2))-1);
                                 } else if (getChecked.contains("Ellipse")){
                                     list.set(3, (list.get(3))-1);
-                                }
+                                } 
 
                                 figs.remove(last_focus);
+                                focus_but = null;
                                 
-                            }
+                            } //else if (focus_but.idx == 5 && last_focus == null) focus_but = null;
                             focus = null;
                             last_focus = null;
                         }
@@ -161,33 +178,50 @@ class IfaceFrame extends JFrame {
 	                    prevPt = currentPt;
 	                    repaint();
         			}
-        		}
+        		};
+
+                public void mouseMoved(MouseEvent evt){
+                    x1 = evt.getX();
+                    y1 = evt.getY();
+                }
         	}
         );
         
         this.addKeyListener (
             new KeyAdapter() {
                 public void keyPressed (KeyEvent evt) {
-                    if (evt.getKeyChar() == 'r') {
+                    if (evt.getKeyChar() == 'p') {
+                        if (positionMouse) positionMouse = false; else positionMouse = true;
+
+                    } else if (evt.getKeyChar() == 'r') {
                         int x = rand.nextInt(300);
                         int y = rand.nextInt(300); 
-                        int w = rand.nextInt(50);
-                        int h = rand.nextInt(50);
+                        int w = rand.nextInt(50)+20;
+                        int h = rand.nextInt(50)+20;
                         int rFill = rand.nextInt(255);
                         int gFill = rand.nextInt(255);
                         int bFill = rand.nextInt(255);
                         int rDraw = rand.nextInt(255);
                         int gDraw = rand.nextInt(255);
                         int bDraw = rand.nextInt(255);
-                        figs.add(new Rect(x,y, w,h, rFill,gFill,bFill, rDraw,gDraw,bDraw));
+                        
+
+                        if (positionMouse) {
+                            //figs.get(figs.size()-1).x = x1- (figs.get(figs.size()-1).w)/2;
+                            //figs.get(figs.size()-1).y = y1- (figs.get(figs.size()-1).h)/2;
+                            figs.add(new Rect(x1-w/2,y1-h/2, w,h, rFill,gFill,bFill, rDraw,gDraw,bDraw));
+                        } else {
+                            figs.add(new Rect(x,y, w,h, rFill,gFill,bFill, rDraw,gDraw,bDraw));
+                        }
+
                         list.set(0, (list.get(0))+1);
                     }
 
                     else if (evt.getKeyChar() == 'e') {
                         int x = rand.nextInt(300);
                         int y = rand.nextInt(300);
-                        int w = rand.nextInt(50);
-                        int h = rand.nextInt(50);
+                        int w = rand.nextInt(50)+20;
+                        int h = rand.nextInt(50)+20;
                         int rFill = rand.nextInt(255);
                         int gFill = rand.nextInt(255);
                         int bFill = rand.nextInt(255);
@@ -195,15 +229,20 @@ class IfaceFrame extends JFrame {
                         int gDraw = rand.nextInt(255);
                         int bDraw = rand.nextInt(255);
 
-                        figs.add(new Ellipse(x,y, w,h, rFill,gFill,bFill, rDraw,gDraw,bDraw));
+                        if (positionMouse) {
+                            figs.add(new Ellipse(x1-w/2,y1-h/2, w,h, rFill,gFill,bFill, rDraw,gDraw,bDraw));
+                        } else {
+                            figs.add(new Ellipse(x,y, w,h, rFill,gFill,bFill, rDraw,gDraw,bDraw));
+                        }
+
                         list.set(3, (list.get(3))+1);
                     }
 
                     else if (evt.getKeyChar() == 'a') {
                     	int x = rand.nextInt(300);
                         int y = rand.nextInt(300);
-                        int w = rand.nextInt(50);
-                        int h = rand.nextInt(50);
+                        int w = rand.nextInt(50)+20;
+                        int h = rand.nextInt(50)+20;
                         int startAngle = rand.nextInt(360);
                         int endAngle = rand.nextInt(360);
                         int rFill = rand.nextInt(255);
@@ -213,19 +252,29 @@ class IfaceFrame extends JFrame {
                         int gDraw = rand.nextInt(255);
                         int bDraw = rand.nextInt(255);
 
-                    	figs.add(new Arc(x,y, w,h, startAngle,endAngle, rFill,gFill,bFill, rDraw,gDraw,bDraw));
+                        if (positionMouse) {
+                            figs.add(new Arc(x1-w/2,y1-h/2, w,h, startAngle,endAngle, rFill,gFill,bFill, rDraw,gDraw,bDraw));
+                        } else {
+                            figs.add(new Arc(x,y, w,h, startAngle,endAngle, rFill,gFill,bFill, rDraw,gDraw,bDraw));
+                        }
+
                         list.set(1, (list.get(1))+1);
 
                     } else if (evt.getKeyChar() == 'l'){
                     	int x = rand.nextInt(350);
                         int y = rand.nextInt(350);
-                        int w = rand.nextInt(50);
-                        int h = rand.nextInt(50);
+                        int w = rand.nextInt(50)+5;
+                        int h = rand.nextInt(50)+5;
                         int rDraw = rand.nextInt(255);
                         int gDraw = rand.nextInt(255);
                         int bDraw = rand.nextInt(255);
 
-                        figs.add(new Line(x,y,w,h, rDraw,gDraw,bDraw));
+                        if (positionMouse) {
+                            figs.add(new Line(x1,y1, w,h, rDraw,gDraw,bDraw));
+                        } else {
+                            figs.add(new Line(x,y, w,h, rDraw,gDraw,bDraw));
+                        }
+
                         list.set(2, (int)((list.get(2))+1));
                     }
 
@@ -251,7 +300,7 @@ class IfaceFrame extends JFrame {
                 		focus.h = focus.h + 1;
                 	}
                 	else if ((evt.getKeyChar() == '-') && (focus != null)){
-                		if ((focus.w != 0) || (focus.h != 0)){
+                		if ((focus.w != 10) || (focus.h != 10)){
                 			focus.w = focus.w - 1;
                 			focus.h = focus.h - 1;
                 		}
@@ -319,6 +368,27 @@ class IfaceFrame extends JFrame {
         }
         g.drawImage(img, 26,225, null);
         
+        if (positionMouse) {
+            g.setFont(new Font("TimesRoman", Font.BOLD, 10));
+            g.setColor(Color.black);
+            buttonMouse.rFill = 103;
+            buttonMouse.gFill = 250;
+            buttonMouse.bFill = 142;
+
+            g.drawString("ON", 40,284);
+            buttonMouse.paint(g, false);
+        } else {
+            g.setColor(Color.black);
+            g.setFont(new Font("TimesRoman", Font.BOLD, 10));
+            buttonMouse.rFill = 255;
+            buttonMouse.gFill = 0;
+            buttonMouse.bFill = 0;
+
+            g.drawString("OFF", 40,284);
+            buttonMouse.paint(g, false);
+        }
+        
+
         g.setColor(Color.white);
         g.setFont(new Font("TimesRoman", Font.BOLD, 17));
 
